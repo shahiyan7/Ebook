f3acd752c5bd2662c065fc8596a3506363c51d75
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-15 18:23:47 +0530

    added scrollable container using pageView

---
+++ lib/Screens/my_home_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:ebook_ui/constant/app_colors.dart' as AppColors;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.background,
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Container(
+                  margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.menu),
+                      Row(
+                        children: [
+                          Icon(Icons.search),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          Icon(Icons.notifications),
+                        ],
+                      )
+                    ],
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Popular Books",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: 180,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        left: -50,
+                        right: 0,
+                        child: Container(
+                          height: 180,
+                          child: PageView.builder(
+                              itemCount: 5,
+                              controller: PageController(viewportFraction: 0.8),
+                              itemBuilder: (BuildContext context, int index) {
+                                return Container(
+                                  margin: EdgeInsets.only(left: 20.0),
+                                  height: 180,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage("img/pic1.png"))),
+                                );
+                              }),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
